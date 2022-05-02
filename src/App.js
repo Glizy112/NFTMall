0542d6c80153cc4103165ee683f9cb5feb3c41f4
Glizy112 <77720813+Glizy112@users.noreply.github.com>
2022-05-03 00:06:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Download, Features, SectionWrapper } from './components';
+import assets from './assets';
+import styles from './styles/Global';
 
-function App() {
+const App =()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper 
+        title="Your personal store of NFTs. Start to Grow."
+        description="Buy, Store, Collect NFTs, Exchange &amp; Earn Crypto. Explore with 10+ million friends using NFT Mall"
+        showBtn
+        mockupImg = {assets.homeHero}  
+        banner="banner"
+      />
+      <SectionWrapper 
+        title="Minimal User Interface Marketplace"
+        description="Experience smooth buttery UI of NFT Mall. Smooth constant colors of a fluent UI design."
+        mockupImg = {assets.homeCards}  
+        reverse
+      />
+      <Features
+        banner ="banner03"
+      />
+      <SectionWrapper 
+        title="Deployment"
+        description="NFT Mall is built using Expo which runs natively for all devices."
+        mockupImg = {assets.feature}  
+        reverse
+      />
+      <SectionWrapper 
+        title="Getting creative to showcase store"
+        description="The app contains two screens. The first one lists all NFTs and the second one have the details about specific NFT."
+        mockupImg = {assets.mockup}  
+        banner = "banner02"
+      />
+      <Download/>
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}> Made with ❤️ by {" "}
+          <span className="bold"> Engineers </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
